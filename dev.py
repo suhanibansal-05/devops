91a66b155438556e2c53e3983e44d795953d7ef5
suhani Bansal <suhanib848402@gmail.com>
2025-07-06 18:34:34 +0530

    push

---
+++ dev.py
@@ -0,0 +1,13 @@
+from flask import flask
+
+app = Flask(_name_)
+
+@app.route("/info")
+
+def myinfo():
+	return "i am suhani hi"
+@app.route("/phone")
+def myphone():
+	return "8484026911"
+
+app.run(host="0.0.0.0")
